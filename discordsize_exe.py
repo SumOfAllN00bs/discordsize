8c5436175184ba3b42d3f873f0f18abb80ca8b8e
SumOfAllN00bs <DiscordShards@gmail.com>
2024-10-13 02:48:29 +1300

    Add target size in MB
    Add target size functionality
    Improve file search
    Fix exiting too fast when error

--- discordsize_exe.py
+++ discordsize_exe.py
@@ -10,11 +10,16 @@ with_scale = False
 targetsize = 10
 
 def select_file():
-    filename = filedialog.askopenfilename(filetypes=[("Video files", "*.mp4 *.avi *.mov *.mkv *.flv *.gif *.gifv *.webm *.ogv *.opus *.mpeg *.3gp *.wmv")])
+    if getattr(sys, 'frozen', False):
+        application_path = os.path.dirname(sys.executable)
+    elif __file__:
+        application_path = os.path.dirname(__file__)
+    filename = filedialog.askopenfilename(initialdir=application_path, filetypes=[("Video files", "*.mp4 *.avi *.mov *.mkv *.flv *.gif *.gifv *.webm *.ogv *.opus *.mpeg *.3gp *.wmv")])
     videopath.delete(0, tk.END)
     videopath.insert(0, filename)
 
 def run_script():
+    targetsize = int(targetmb.get())
     input_video = Path(videopath.get())
     override_audio_rate = int(audio_rate.get())
     with_scale = scale.get()
@@ -48,9 +53,10 @@ def run_script():
         target_minsize = (original_audio_rate * duration) / 8192
         is_minsize = target_minsize < targetsize
         if not is_minsize:
+            tk.messagebox.showerror("File too small", f"file {input_video} is too small (usually cause audio takes too much room)")
             print(f"Target size {(targetsize - 1)}MB is too small!")
             print(f"Try values larger than {round(target_minsize, 2)}MB")
-            sys.exit()
+            return
         vbitrate = int((targetsize * 8192) / (1.048576 * duration)) - original_audio_rate
     else:
         vbitrate = int((targetsize * 8192) / (1.048576 * duration))
@@ -79,33 +85,38 @@ def scale_toggle():
 root = tk.Tk()
 root.title("Discord Video Resizer")
 
-ttk.Label(root, text="Input Video Path").grid(row=0, column=0, padx=5, pady=5)
-videopath = ttk.Entry(root, width=50)
-videopath.grid(row=0, column=1, padx=5, pady=5)
-ttk.Button(root, text="Browse", command=select_file).grid(row=0, column=2, padx=5, pady=5)
+ttk.Label(root, text="Target size in MB (whole integer)").grid(row=0, column=0, padx=5, pady=5)
+targetmb = ttk.Entry(root)
+targetmb.grid(row=0, column=1, padx=5, pady=5)
+targetmb.insert(0, "10")
 
-ttk.Label(root, text="Force Audio Rate (0 = don't force, 32 64 128 good options)").grid(row=1, column=0, padx=5, pady=5)
+ttk.Label(root, text="Input Video Path").grid(row=1, column=0, padx=5, pady=5)
+videopath = ttk.Entry(root, width=50)
+videopath.grid(row=1, column=1, padx=5, pady=5)
+ttk.Button(root, text="Browse", command=select_file).grid(row=1, column=2, padx=5, pady=5)
+
+ttk.Label(root, text="Force Audio Rate (0 = don't force, 32 64 128 good options)").grid(row=2, column=0, padx=5, pady=5)
 audio_rate = ttk.Entry(root)
-audio_rate.grid(row=1, column=1, padx=5, pady=5)
+audio_rate.grid(row=2, column=1, padx=5, pady=5)
 audio_rate.insert(0, "0")
 
 scale = tk.BooleanVar()
 scale_check = tk.Checkbutton(root, text="Enable specifying video scale", variable=scale, command=scale_toggle)
-scale_check.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+scale_check.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
 
-ttk.Label(root, text="Video Width").grid(row=3, column=0, padx=5, pady=5)
+ttk.Label(root, text="Video Width").grid(row=4, column=0, padx=5, pady=5)
 vidwidth = ttk.Entry(root, state="disabled")
-vidwidth.grid(row=3, column=1, padx=5, pady=5)
+vidwidth.grid(row=4, column=1, padx=5, pady=5)
 
-ttk.Label(root, text="Video Height").grid(row=4, column=0, padx=5, pady=5)
+ttk.Label(root, text="Video Height").grid(row=5, column=0, padx=5, pady=5)
 vidheight = ttk.Entry(root, state="disabled")
-vidheight.grid(row=4, column=1, padx=5, pady=5)
+vidheight.grid(row=5, column=1, padx=5, pady=5)
 
 run_button = ttk.Button(root, text="Run", command=run_script)
-run_button.grid(row=5, column=0, columnspan=2, pady=10)
+run_button.grid(row=6, column=0, columnspan=2, pady=10)
 
 progress_label = ttk.Label(root, text="")
-progress_label.grid(row=6, column=0, columnspan=2, pady=5)
+progress_label.grid(row=7, column=0, columnspan=2, pady=5)
 
 root.mainloop()
 
